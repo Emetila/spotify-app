6ab76b2e95f3188438fb4b6699f95ef661c5a29b
Emetila <emetila83@gmail.com>
2024-03-11 13:32:06 +0100

    Implemented hero section design

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { LandingPageHeader } from './components/Header/LandingPageHeader'
+import { Hero } from './components/Hero/index.jsx';
 
 export function App() {
   return (
     <div>
       <LandingPageHeader />
+      <Hero />
     </div>
   );
 }
