3a724ea33d2561bb5ccfb32b02a1240e74477895
Emetila <emetila83@gmail.com>
2024-03-24 02:36:57 +0100

    Implemented complete footer desifn abd added hover effects to links

---
+++ src/app/components/Footer/data.js
@@ -0,0 +1,58 @@
+export const companyMenu = [
+    {
+        name: "About",
+        url: "$About",
+    },
+    {
+        name: "Jobs",
+        url: "#Jobs",
+    },
+    {
+        name: "For the Record",
+        url: "#FortheRecord",
+    }
+]
+export const communitiesMenu = [
+    {
+        name: "For Artists",
+        url: "#ForArtists",
+    },
+    {
+        name: "Developers",
+        url: "#Developers",
+    },
+    {
+        name: "Advertising",
+        url: "#Advertising",
+    },
+    {
+        name: "Investors",
+        url: "#Investors",
+    },
+    {
+        name: "Vendors",
+        url: "#Vendors",
+    },
+]
+export const usefulLinkMenu = [
+    {
+        name: "Support",
+        url: "#Support",
+    },
+    {
+        name: "Web Player",
+        url: "#WebPlayer",
+    },
+    {
+        name: "Free Mobile App",
+        url: "#FreeMobileApp",
+    }
+]
+
+export const legalUrls = [
+    { url: '#Legal', name: 'Legal'},
+    { url: '#PrivacyCenter', name: 'Privacy Center'},
+    { url: '#PrivacyPolicy', name: 'Privacy Policy'},
+    { url: '#Cookies', name: 'Cookies'},
+    { url: '#AboutAds', name: 'About Ads'},
+]
--- src/app/components/Footer/index.jsx
+++ src/app/components/Footer/index.jsx
@@ -4,12 +4,25 @@ import { ReactComponent as Instagram } from "../../assets/footericon/instagram.s
 import { ReactComponent as Twitter } from "../../assets/footericon/twitter.svg";
 import { ReactComponent as EarthIcon } from "../../assets/footericon/earth.svg";
 import Logo from '../Logo/Logo';
+import { companyMenu, communitiesMenu, usefulLinkMenu, legalUrls } from './data';
 
 export default function Footer () {
     return (<footer className={style.footer}>
         <div className={style.menu}>
             <div>
-                <Logo useWhite/>
+                <div>
+                    <Logo useWhite/>
+                </div>
+                <div className={style.flex}>
+                    <Menu title="Company" options={companyMenu} />
+                    <Menu title="Communities" options={communitiesMenu} />
+                    <Menu title="Useful Links" options={usefulLinkMenu} />
+                </div>
+            </div>
+            <div className={style['legal-urls']}>
+                {
+                   legalUrls.map(({ url, name}) => <a href={url}>{name}</a>)
+                }
             </div>
         </div>
 
@@ -19,10 +32,23 @@ export default function Footer () {
                 <Twitter />
                 <Facebook />
             </div>
-            <div>
+            <div className={style.copy}>
                 <p><EarthIcon />India (English)</p>
                 <p>© 2023 Spotify AB</p>
             </div>
         </div>
     </footer>)
+}
+
+export const Menu = ({ title, options}) => {
+    return (<nav className={style.nav}>
+        <h5>{title || <>&nbsp;</>} </h5>
+        <ul>
+        {
+            options && options.map((option)=>{
+                return (<li><a href={option.url}>{option.name}</a></li>)
+            })
+        }
+        </ul> 
+    </nav>)
 }
